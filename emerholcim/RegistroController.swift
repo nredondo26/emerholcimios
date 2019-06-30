73d9846e4455985e76da0332e95ca30a26ced201
Nelson Enriquee Redono barros <nredondo26@gmail.com>
2019-06-29 20:02:43 -0500

    se agrega drowdolis en registro y repara algunos todos las advertencias

--- emerholcim/RegistroController.swift
+++ emerholcim/RegistroController.swift
@@ -9,20 +9,22 @@ import UIKit
 import Alamofire
 import Toaster
 import Toast_Swift
+import iOSDropDown
 
-
-class RegistroController: UIViewController,UIImagePickerControllerDelegate, UINavigationControllerDelegate,UIPickerViewDataSource,UIPickerViewDelegate {
+class RegistroController: UIViewController,UIImagePickerControllerDelegate, UINavigationControllerDelegate{
     
     
     @IBOutlet weak var nombre: UITextField!
     @IBOutlet weak var apellidos: UITextField!
-    @IBOutlet weak var zona: UITextField!
-    @IBOutlet weak var cargos: UITextField!
+    @IBOutlet weak var zona: DropDown!
+    @IBOutlet weak var cargos: DropDown!
     @IBOutlet weak var email: UITextField!
     @IBOutlet weak var password: UITextField!
     @IBOutlet weak var codempleado: UITextField!
     @IBOutlet weak var imagen: UIImageView!
-    @IBOutlet weak var piker: UIPickerView!
+    
+    @IBOutlet weak var vistazona: UIView!
+    
     
     let plantas = ["Bello",
                    "Buga",
@@ -39,8 +41,7 @@ class RegistroController: UIViewController,UIImagePickerControllerDelegate, UINa
                    "La Flor",
                    "Pipiral"]
     
-   /* let listacargos = ["Cargos",
-                  "Jefe de Brigada",
+    let listacargos = ["Jefe de Brigada",
                   "Jefe de Brigada",
                   "Jefe de Brigada Zona I",
                   "Jefe de Brigada Zona III",
@@ -73,9 +74,7 @@ class RegistroController: UIViewController,UIImagePickerControllerDelegate, UINa
                   "rescate",
                   "Otros",
                   "Visitante",
-                  "Trabajador no Holcim"]*/
-    
-    var estadopiker : Int = 0
+                  "Trabajador no Holcim"]
     
     var valor: String?
     var valorr: Int?
@@ -84,7 +83,10 @@ class RegistroController: UIViewController,UIImagePickerControllerDelegate, UINa
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        piker.isHidden = true
+        
+        zona.optionArray = plantas
+        cargos.optionArray = listacargos
+       
         imagePicker.delegate = self
     }
     
@@ -107,33 +109,6 @@ class RegistroController: UIViewController,UIImagePickerControllerDelegate, UINa
         return zonaf!
     }
     
-    @IBAction func oultar(_ sender: Any) {
-        estadopiker = 0
-        piker.isHidden = true
-    }
-    
-    @IBAction func mostrarlista(_ sender: Any) {
-        estadopiker = 1
-        piker.isHidden = false
-    }
-    
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-       return plantas[row]
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return plantas.count
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        
-      zona.text = plantas[row]
-
-    }
 
     @IBAction func cargarimagen(_ sender: Any) {
         imagePicker.allowsEditing = false
@@ -196,7 +171,7 @@ class RegistroController: UIViewController,UIImagePickerControllerDelegate, UINa
         
         let nombrer = nombre.text!
         let apellidosr = apellidos.text!
-        let zonar = zona.text!
+      let zonar = zona.text!
         let cargosr = cargos.text!
         let emailr = email.text!
         let passwordr = password.text!
---
+++ emerholcim/prueba.swift
@@ -0,0 +1,24 @@
+//
+//  prueba.swift
+//  emerholcim
+//
+//  Created by nerb2019 on 6/29/19.
+//
+
+import UIKit
+
+
+class prueba: UIViewController {
+
+    
+    
+    
+    override func viewDidLoad() {
+    super.viewDidLoad()
+        
+       
+        
+    
+}
+
+}
